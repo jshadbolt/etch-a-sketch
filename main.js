961e021778f21284ff9dcde1cf42dc4f42e5872b
jshadbolt <146521761+jshadbolt@users.noreply.github.com>
2024-01-10 21:03:52 +1300

    add color picker

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const DEFAULTSIZE = 16
-const DEFAULTCOLOUR = '#000'
+const DEFAULTcolor = '#000000'
 const canvas = document.querySelector('.canvas')
 const toggleGridButton = document.getElementById('toggle-grid-button')
 const clearCanvasButton = document.getElementById('clear-canvas-button')
 const colorPickerButton = document.getElementById('color-picker-button')
-let slider = document.getElementById('slider')
-let sliderOutput = document.getElementById('slider-output')
-createSquares(DEFAULTSIZE)
+const swatch = document.getElementById('swatch')
+const slider = document.getElementById('slider')
+const sliderOutput = document.getElementById('slider-output')
+let currentcolor = DEFAULTcolor
 
 var mouseDown = 0;
 document.body.onmousedown = function() { 
@@ -23,24 +24,12 @@ function createSquares(gridSize) {
         for (let j = 0; j < gridSize; j++) {
             let square = document.createElement('div')
             square.className = 'square'
-            square.classList.add('grid')
             square.classList.toggle('grid')
-            addDraw(square)
             column.appendChild(square)
         }
         canvas.appendChild(column)
     }
-}
-
-function addDraw(square) {
-    square.addEventListener('mouseover', () => {
-        if (mouseDown) {
-        square.style.backgroundColor = 'black'
-        }
-    })
-    square.addEventListener('mousedown', () => {
-        square.style.backgroundColor = 'black'
-    })
+    addDraw()
 }
 
 function destroyGrid() {
@@ -65,15 +54,42 @@ slider.addEventListener('input', () => {
 })
 
 toggleGridButton.addEventListener('click', () => {
-    let squares = document.querySelectorAll('.square')
-    squares.forEach(square => {
-        square.classList.toggle('grid')
-    })
+    toggleGrid()
 })
 
 clearCanvasButton.addEventListener('click', () => {
     newGrid();
 })
 
+function toggleGrid() {
+    let squares = document.querySelectorAll('.square')
+    squares.forEach(square => {
+        square.classList.toggle('grid')
+    })
+}
+
+function updateCurrentColor() {
+    currentcolor = swatch.value
+    console.log(currentcolor)
+}
+
+function addDraw() {
+    let squares = Array.from(document.querySelectorAll('.square'))
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            if (mouseDown) {
+                square.style.backgroundColor = currentcolor
+            }
+        }) 
+        square.addEventListener('mousedown', () => {
+            square.style.backgroundColor = currentcolor
+        })    
+    })
+}
+
+swatch.addEventListener('input', () => {
+    updateCurrentColor()
+})
 
 
+createSquares(DEFAULTSIZE)
